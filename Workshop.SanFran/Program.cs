da3fea812f437e68693ae8a2f37a8618cae72de6
Ayende Rahien <Ayende@ayende.com>
2018-02-08 15:34:06 -0800

    Async work and queries

--- Workshop.SanFran/Program.cs
+++ Workshop.SanFran/Program.cs
@@ -5,12 +5,19 @@ using Raven.Client.Documents.Commands.Batches;
 using Workshop.SanFran.Orders;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Queries;
 
 namespace Workshop.SanFran
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            Main().GetAwaiter().GetResult();
+        }
+
+        static async Task Main()
         {
             using(var store = new DocumentStore
             {
@@ -26,18 +33,26 @@ namespace Workshop.SanFran
             {
                 store.Initialize();
 
-                using (var session = store.OpenSession())
+                using (var session = store.OpenAsyncSession())
                 {
+                    
                     var totals = from o in session.Query<Order>()
-                               group o by o.ShipTo.Country into g
-                               select new
-                               {
-                                   Country = g.Key,
-                                   Count = g.Count()
-                               };
+                                 where o.ShipTo.City == "London"
+                                 let e = RavenQuery.Load<Employee>(o.Employee)
+                                 let manager = RavenQuery.Load<Employee>(e.ReportsTo)
+                                 let name = (Func<Employee, string>)(e => e.FirstName + " " + e.LastName)
+                                 select new
+                                 {
+                                     o.Company,
+                                     EmpName = name(e),
+                                     ManagerName = name(manager)
+                                 };
 
+                    Console.WriteLine(totals);
 
-                    foreach (var item in totals)
+                    var results = await totals.Take(2).ToListAsync();
+
+                    foreach (var item in results)
                     {
                         Console.WriteLine(item);
                     }
