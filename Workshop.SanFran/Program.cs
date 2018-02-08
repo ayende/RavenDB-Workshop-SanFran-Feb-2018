26dbfd73e79f7bf548dfbc90a9bebcb618e8f72f
Ayende Rahien <Ayende@ayende.com>
2018-02-08 09:43:17 -0800

    Initial code

---
+++ Workshop.SanFran/Program.cs
@@ -0,0 +1,29 @@
+﻿using Raven.Client.Documents;
+using System;
+
+namespace Workshop.SanFran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var store = new DocumentStore
+            {
+                Database = "Bio",
+                Urls = new[] 
+                {
+                    "http://localhost:8080"
+                }
+            })
+            {
+                store.Initialize();
+
+                using (var session = store.OpenSession())
+                {
+                    var nancy = session.Load<dynamic>("employees/1-A");
+                    Console.WriteLine(nancy.FirstName + " " + nancy.LastName);
+                }
+            }
+        }
+    }
+}
